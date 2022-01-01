eab1850b87c4be64f5c5f6ea75915639019a06da
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-01 12:05:12 +0200

    login account

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import * as serviceWorker from "./serviceWorker";
 import { PoliticsProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// domain: dev-25s5gpi2.us.auth0.com
+// Client id: n2eTOkJYrEXLtv4PRePsSQcnaYkUxPWt
+
 ReactDOM.render(
   <React.StrictMode>
-    <PoliticsProvider>
-      <App />
-    </PoliticsProvider>
+    <Auth0Provider
+      domain="dev-25s5gpi2.us.auth0.com"
+      clientId="n2eTOkJYrEXLtv4PRePsSQcnaYkUxPWt"
+      redirectUri={window.location.origin}
+    >
+      <PoliticsProvider>
+        <App />
+      </PoliticsProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
