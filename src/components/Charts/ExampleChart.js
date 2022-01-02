38e5cf2cbd8bfff271fd1434a6ad3a511c72ae0e
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-02 18:45:19 +0200

    charts set up

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -80,11 +80,9 @@ const chartConfigs = {
   },
 };
 
+const ChartComponent = () => {
+  return <ReactFC {...chartConfigs} />;
+};
 // STEP 4 - Creating the DOM element to pass the react-fusioncharts component
-class App extends React.Component {
-  render() {
-    return <ReactFC {...chartConfigs} />;
-  }
-}
 
-export default App;
+export default ChartComponent;
