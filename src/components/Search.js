8238ba51dc595fe15f257d51174eafad07f7d62b
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-02 11:10:08 +0200

    search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  const [user,setUser] = React.useState('')
+  const [user, setUser] = React.useState("");
   const handleSubmit = (e) => {
-    console.log(user)
-  }
-  return <h2>search component</h2>;
+    e.preventDefault()
+    console.log(user);
+
+
+    setUser('')
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="Enter a name"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -14,6 +14,29 @@ const PoliticsProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [loading, setIsLoading] = useState(false);
+
+  // const checkRequest = () => {
+  //   axios(`${rootUrl}/rate_limit`)
+  //     .then((data) => {})
+  //     .catch((err) => console.log(err));
+  // };
+
+  const checkRequest = async () => {
+    try {
+      const {data} = await axios.get(rootUrl);
+      let {rate:{remaining}} = data
+      setRequests(remaining)
+      if(remaining === 0) {
+        
+      }
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    checkRequest();
+  }, []);
   return (
     <PoliticsContext.Provider value={{ githubUser, repos, followers }}>
       {children}
